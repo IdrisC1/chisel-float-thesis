e8e0992adbe7ba4eeb84782ec7580e2b951d08ed
Idris <idris.christiaensen@student.kuleuven.be>
2025-09-28 16:17:00 +0200

    test

--- src/main/resources/fp_add.sv
+++ src/main/resources/fp_add.sv
@@ -1,4 +1,4 @@
-// Copyright 2019 ETH Zurich and University of Bologna.
+ // Copyright 2019 ETH Zurich and University of Bologna.
 // Solderpad Hardware License, Version 0.51, see LICENSE for details.
 // SPDX-License-Identifier: SHL-0.51
 // Author: Stefan Mach <smach@iis.ee.ethz.ch>
